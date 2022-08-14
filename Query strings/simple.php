8dba99f795040d78b71c10662f71a173a5b1f00c
Your Name <roshikanayanadhara@gmail.com>
2022-08-14 23:05:37 +0530

    .

--- Query strings/simple.php
+++ Query strings/simple.php
@@ -1,6 +1,8 @@
 <?php
 $firstName = "John";
 $age = "34";
+// $queryString = "firstName=$firstName&amp;age=$age";
+// echo "<p> <a href='moreinfo.php?" . $queryString . "' > Find out more info on this person </a> </p> ";
 $queryString = "firstName=$firstName&amp;age=$age";
 echo "<p> <a href='moreinfo.php?" . $queryString . "' > Find out more info on this person </a> </p> ";
 ?>
